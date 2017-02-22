b06ae2510c0cb05a0643a9b0ef31cc52104a8821
lynch16 <will.lynch91@gmail.com>
2017-02-22 10:46:22 -0500

    building states service

--- client/js/app/app.js
+++ client/js/app/app.js
@@ -2,23 +2,13 @@ angular
     .module('app', ['ui.router'])
     .config(function($stateProvider, $urlRouterProvider){
       $stateProvider
-        .state('home', {
-          url: '/home',
-          templateUrl: 'views/home.html',
-          controller: 'HomeController as home',
-          resolve: {
-            posts: function(PostsService){
-              return PostsService.getTopStories();
-            }
-          }
-        })
         .state('state', {
           url: '/state/:id',
           templateUrl: 'views/state.html',
           controller: 'StateController as state',
           resolve: {
-            post: function($stateParams, PostsService){
-              return PostsService.getStory($stateParams.id);
+            state: function($stateParams, StatesService){
+              return StatesService.getState($stateParams.id);
             }
           }
         })
--- client/js/app/services/StatesSerice.js
+++
@@ -1,9 +0,0 @@
-function StatesService($http){
-  this.getStateLegislators = function(id){
-    return $http.get('https://localhost:3000/states/' + id);
-  }
-}
-
-angular
-  .module('app')
-  .service('StatesService', StatesService)

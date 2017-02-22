5e712966bd733a16cb8b0e5a3de1fa359aaf362f
lynch16 <will.lynch91@gmail.com>
2017-02-22 11:44:53 -0500

    have frontend talking to backend and api calls to open states working

--- client/js/app/app.js
+++ client/js/app/app.js
@@ -2,6 +2,17 @@ angular
     .module('app', ['ui.router'])
     .config(function($stateProvider, $urlRouterProvider){
       $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/home.html',
+        controller: 'HomeController as home',
+        resolve: {
+          states: function(StatesService){
+            return StatesService.getAllStates();
+            }
+          }
+        })
+
         .state('state', {
           url: '/state/:id',
           templateUrl: 'views/state.html',
--- client/js/app/controllers/HomeController.js
+++ client/js/app/controllers/HomeController.js
@@ -1,8 +1,8 @@
-function HomeController() {
+function HomeController(states) {
   //stories contains each story returned from http get within stories.data
 
   var ctrl = this;
-
+  ctrl.states = states.data
 }
 
 angular

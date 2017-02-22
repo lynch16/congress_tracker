799f6c68d5112e023b2cf027d280ea6002616194
lynch16 <will.lynch91@gmail.com>
2017-02-22 16:52:22 -0500

    add certain data formatting to server side

--- server/app/models/legislator.rb
+++ server/app/models/legislator.rb
@@ -8,12 +8,20 @@ class Legislator
 
   def self.search_location(lat, long)
     response = self.get("/legislators/geo/?lat=#{lat}&long=#{long}")
-    response.collect {|l| new(l) }
+    response.collect {|l| new(l) }.compact
   end
 
   def initialize(json)
-    self.name = json["full_name"]
-    # self.state = State.find_state(json["state"])
-    self.chamber = json["chamber"]
+    if json["active"] == true
+      self.name = json["full_name"]
+      self.state = State.find_state(json["state"]).name
+      if json["chamber"] == 'upper'
+        self.chamber = 'Senate'
+      elsif json["chamber"] == 'lower'
+        self.chamber = 'House of Representatives'
+      else
+        self.chamber = 'Other'
+      end
+    end
   end
 end

49683d27c087375c339f3bd7aaaa6510992f9b7b
lynch16 <will.lynch91@gmail.com>
2017-02-28 11:24:09 -0500

    removed log used for testing

--- client/js/app/controllers/LegislatorsController.js
+++ client/js/app/controllers/LegislatorsController.js
@@ -1,7 +1,6 @@
 function LegislatorsController(legislators){
   var vm = this;
   vm.legislators = legislators.data;
-  console.log(vm.legislators);
   vm.availableOptions = [
     {value: '', name: 'All'},
     {value: 'Senate', name: 'Senate'},

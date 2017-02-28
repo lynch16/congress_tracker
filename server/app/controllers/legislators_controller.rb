ad2bbcaec73b9b6b5f4f0476d63cc2d3fcdac8a7
lynch16 <will.lynch91@gmail.com>
2017-02-28 08:27:12 -0500

    added up/downvote routes and actions

--- server/app/controllers/legislators_controller.rb
+++ server/app/controllers/legislators_controller.rb
@@ -3,4 +3,16 @@ class LegislatorsController < ApplicationController
     legislators = Legislator.search_location(params[:lat], params[:long])
     render json: legislators, status: :ok
   end
+
+  def upvote
+    a = Legislator.find_by(params[:id])
+    a.upvote
+    render json: a, status: :ok
+  end
+
+  def downvote
+    a = Legislator.find_by(params[:id])
+    a.downvote
+    render json: a, status: :ok
+  end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root  'home#index'
   resources :states
   resources :legislators
-  
+  get 'legislators/:id/upvote', to: 'legislators#upvote'
+  get 'legislators/:id/downvote', to: 'legislators#downvote'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
